c6dd00a8d164f3f834eef8543f474b459d385cbb
Tyler Alioto <tyler.alioto@gmail.com>
2022-07-21 13:54:46 +0200

    new scaffolds2contigs is now called format_assembly.pl

--- format_assembly.pl
+++ format_assembly.pl
@@ -10,62 +10,89 @@ my $num_n = 1;
 my $species = '';
 my $taxid = '';
 my $assembly_name = 0;
+my $outfilebase = 0;
+my $prefix = '';
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my @abbr = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
 my $realyear = 1900+$year;
 my $date = "$mday-$abbr[$mon]-$realyear";
 my $center = "CNAG";
 my $description = "";
-my $min_scaff_size = 1000;
-my $min_contig_size = 100;
+my $min_scaff_size = 20;
+my $min_contig_size = 20;
 my $sp = 0;
 my $cp = 0;
 my $lookuptable = 1;
-my $keep = 0;
+my $keep = 1;
 my $sort = 1;
+my $gap_cutoff = 0;
+my $rename_tsv  = 0;
 GetOptions(
 	   'f|s|i:s'        => \$fname,
-	   'k|keep'         => \$keep,
-	   'ns:s'            => \$num_n,
-	   'name:s'         =>\$assembly_name,
-	   'taxid:s'        =>\$taxid,
-	   'species:s'      =>\$species,
-	   'center:s'       =>\$center,
-	   'date:s'         =>\$date,
-	   'mins:s'         =>\$min_scaff_size,
-	   'minc:s'         =>\$min_contig_size,
+	   'k|keep!'        => \$keep,
+	   'ns:s'           => \$num_n,
+	   'name:s'         => \$assembly_name,
+	   'taxid:s'        => \$taxid,
+	   'species:s'      => \$species,
+	   'center:s'       => \$center,
+	   'date:s'         => \$date,
+	   'mins:s'         => \$min_scaff_size,
+	   'minc:s'         => \$min_contig_size,
 	   'sp:s'           => \$sp,
 	   'cp:s'           => \$cp,
-	   'lookup!'       => \$lookuptable,
-	   'sort!'			=> \$sort
+	   'lookup!'        => \$lookuptable,
+	   'sort!'			    => \$sort,
+		 'g|gap:i'				=> \$gap_cutoff,
+		 'rename:s'       => \$rename_tsv
 	);
 my $options = <<HERE;
-           -f,-s,-i	name of fasta file, required
-           -n		number of Ns on which to break (1)
-           -name	assembly name (filename)
-           -taxid	taxonomy id from NCBI
-           -species	species
-           -center	center (CNAG)
-           -date	date of assembly (current local time)
-           -mins	minimum scaffold size to keep (20)
-           -minc	minimum contig size to keep (20)
+           -f,-s,-i	<file.fasta>  name of fasta file, required
+					 -n	<int>	              number of Ns on which to break (1)
+           -g	<int>	              threshold unestimated gap size  (0)
+           -name <assembly_name>	assembly name used for output filename and seq prefix
+           -taxid	<int>           taxonomy id from NCBI
+           -species	<species>     species
+           -center <center>	      center (CNAG)
+           -date <date>	          date of assembly (current local time)
+           -mins <int>	          minimum scaffold size to keep (20)
+           -minc <int>	          minimum contig size to keep (20)
+					 -sort                  sort scaffolds by size longest to shortest (default)
+					 -nosort                don't sort/original order
+					 -rename <newnames.tsv> tab-delimited text file in format:
+					  												  old_scaff_name <tab> new_scaff_name
+
 HERE
 die "usage: $0 -f <scaffolds.fa> [options]\n$options\n" if !$fname;
 print STDERR "'-n 0' does not make sense. Running with '-n 1'\n" if !$num_n;
 my ($base,$path,$ext) = fileparse($fname,qw(\.fa \.fasta));
 if ($assembly_name){
-  $base = $assembly_name;
+  $prefix = $assembly_name.'_';
+	$outfilebase = $assembly_name;
 }else{
-  $assembly_name = $base;
+  $prefix = '';
+	$outfilebase = $base;
 }
-my $cout = "$base.contigs.fa";
-my $sout = "$base.scaffolds.fa";
-my $offsets = "$base.contigs.offsets";
-my $soffsets = "$base.scaffolds.offsets";
-my $gaps = "$base.scaffolds.gaps.bed";
-my $agp = "$base.agp";
-my $lup = "$base.lookup.txt";
+my $cout = "$outfilebase.contigs.fa";
+my $sout = "$outfilebase.scaffolds.fa";
+my $offsets = "$outfilebase.contigs.offsets";
+my $soffsets = "$outfilebase.scaffolds.offsets";
+my $gaps = "$outfilebase.scaffolds.gaps.bed";
+my $agp = "$outfilebase.agp";
+my $lup = "$outfilebase.lookup.txt";
 my @scaffolds;
+
+my %newnames = ();
+if ($rename_tsv){
+	open (TSV,"<$rename_tsv") or die "Couldn't open $rename_tsv!";
+	while(my $l = <TSV>){
+		chomp $l;
+		my @f = split "\t",$l;
+		$newnames{$f[0]}=$f[1];
+	}
+	close TSV;
+}
+
+
 open IN, "FastaToTbl $fname |";
 my $maxc=1;
 my $nums=0;
@@ -117,14 +144,14 @@ foreach my $scaff (@scaffolds){
   chomp;
   my $sid = $scaff->{id};
   my $seq = $scaff->{seq};
-  my $oldid = $sid; 
+  my $oldid = $sid;
   my $seq_up = uc($seq);
   $seq = $seq_up;
   #print STDERR "$sid\n";
   my $num = $min_contig_size - 1;
   if ($num>0){
-  	$seq=~s/^([^N]{1,$num})N/"N" x (length($1)+1)/ge;  
-  	$seq=~s/N([^N]{1,$num})N/"N" x (length($1)+2)/ge; 
+  	$seq=~s/^([^N]{1,$num})N/"N" x (length($1)+1)/ge;
+  	$seq=~s/N([^N]{1,$num})N/"N" x (length($1)+2)/ge;
   	$seq=~s/N([^N]{1,$num})$/"N" x (length($1)+1)/ge;
   }
   if ($seq=~s/^(N+)//){ ### this creates new  offset
@@ -139,17 +166,33 @@ foreach my $scaff (@scaffolds){
   my $outseq = $seq;
   $outseq=~s/[^ACGT]/N/g;
   next if $outseq!~/[ACGTacgt]/;
-  if ($sid=~/SUPER_/){
+	if($rename_tsv){
+		$keep = 1;
+		my $suffix = '';
+		if($sid =~s/(_unloc)$//){
+				$suffix = '_unloc';
+		}
+		if (exists $newnames{$sid}){
+			$sid = $newnames{$sid};
+		}
+		$sid = $sid.$suffix;
+	}
+	if ($sid=~/SUPER_/){
       $sid=~s/SUPER_/Chr/;
   }
-  $sid = sprintf("%s"."_".$sid,$base);
+	if($keep){
+		$sid = $prefix.$sid;
+	}else{
+		$sid = sprintf("%s"."s%0$sp"."i",$prefix,$scount++);
+	}
+
 
   print SOUT "$sid\t$outseq\n";
   if($lookuptable){print LU "$sid\t$oldid\n";}
   my $count = 1;
   # my @contigs = split /N+/,$seq;
   # foreach my $contig (@contigs){
-    
+
   #   my $cid = sprintf("%s.contig%04d",$sid,$count++);
   #   print COUT "$cid\t$contig\n";
   # }
@@ -164,11 +207,13 @@ foreach my $scaff (@scaffolds){
     if ($element % 2){
       my $gapsize = length($contig_or_gap);
       if ($gapsize == 200){
-	$evidence = "proximity_ligation";$gapsize = 100;$component_type="U";
-      }elsif($gapsize == 100){
-	$evidence = "paired-ends";$gapsize = 100;$component_type="U";
+				$gapsize = 100;
+				$evidence = "proximity_ligation";$component_type="U";
+      }elsif($gapsize > $gap_cutoff){
+				$gapsize = 100;
+				$evidence = "paired-ends";$component_type="U";
       }else{
-	$evidence = "paired-ends";$component_type="N";
+				$evidence = "paired-ends";$component_type="N";
       }
       print AGP join("\t",($sid,$offset+1,$offset+$gapsize, $part_number++,$component_type,$gapsize,'scaffold','yes',$evidence)),"\n";
       $offset+=$gapsize;
@@ -176,7 +221,7 @@ foreach my $scaff (@scaffolds){
       $contig_or_gap=~s/[^ACGTacgt]/N/g; #replace IUPAC ambiguity codes;
       #my $offset = $end - length($contig);
       print GAP  "$sid\t$END\t$offset\n" if $count > 1;;
-      my $cid = sprintf("%sc%0$cp"."d",$sid,$count++);
+      my $cid = sprintf("%s_c%0$cp"."d",$sid,$count++);
       my $contig_len = length($contig_or_gap);
       print COUT "$cid\t$contig_or_gap\n";
       print OFF  "$cid\t$offset\n";
@@ -199,4 +244,3 @@ sub lensort
   {
     $b->{len} <=> $a->{len}
   }
-
