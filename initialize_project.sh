9fb69ebd09c0b7f643fc1335f67498012556da6c
Tyler Alioto <tyler.alioto@gmail.com>
2025-05-22 17:23:33 +0200

    modified initialize_project.sh script

--- initialize_project.sh
+++ initialize_project.sh
@@ -17,8 +17,8 @@ CNAGDATA=$2
 BASE=$PWD;
 mkdir -p $BASE/$SPECIES
 chmod 775 $BASE/$SPECIES
-mkdir -p "$BASE/$SPECIES/assembly"
-chmod 775 "$BASE/$SPECIES/assembly"
+mkdir -p "$BASE/$SPECIES/assembly/asm1"
+chmod 775 "$BASE/$SPECIES/assembly/asm1"
 mkdir -p "$BASE/$SPECIES/analysis"
 chmod 775 "$BASE/$SPECIES/analysis"
 mkdir -p "$BASE/$SPECIES/annotation"
@@ -27,11 +27,15 @@ mkdir -p "$BASE/$SPECIES/assembly/curation"
 chmod 775 "$BASE/$SPECIES/assembly/curation"
 mkdir -p "$BASE/$SPECIES/assembly/mitogenome"
 chmod 775 "$BASE/$SPECIES/assembly/mitogenome"
+mkdir -p "$BASE/$SPECIES/assembly/cobiont"
+chmod 775 "$BASE/$SPECIES/assembly/cobiont"
 mkdir -p "$BASE/$SPECIES/assembly/hic_qc"
 chmod 775 "$BASE/$SPECIES/assembly/hic_qc"
-mkdir -p "$BASE/$SPECIES/data/cnag"
-chmod 775 "$BASE/$SPECIES/data/cnag"
-for i in $CNAGDATA/*; do ln -s $i $BASE/$SPECIES/data/cnag/; done
+mkdir -p "$BASE/$SPECIES/data"
+chmod 775 "$BASE/$SPECIES/data"
+if [ $2 ]; then
+    ln -s $CNAGDATA "$BASE/$SPECIES/data/cnag"
+fi
 mkdir -p "$BASE/$SPECIES/data/external"
 chmod 775 "$BASE/$SPECIES/data/external"
 mkdir -p "$BASE/$SPECIES/data/reference"
